2aa3b4bd23732c7e70e819196a11792bf5be0f80
nqdinh1203 <dinh.nguyen@gihot.vn>
2023-05-16 18:08:26 +0700

    add deploy mock

--- deploy/00-deploy-mocks.ts
+++ deploy/00-deploy-mocks.ts
@@ -2,7 +2,10 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { hardhatArguments, network } from 'hardhat';
 import * as Config from "../scripts/config";
-import {networkConfig} from "../helper-hardhat-config";
+import { developmentChains } from "../helper-hardhat-config";
+
+const DECIMALS = "8"
+const INITIAL_PRICE = "200000000000" // 2000
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     await Config.initConfig();
@@ -12,5 +15,23 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deploy, log } = deployments;
     const { deployer, user } = await getNamedAccounts();
 
-    const chainId = network.config.chainId ? network.config.chainId : 31337;
+    // const chainId = network.config.chainId ? network.config.chainId : 31337;
+
+    if (developmentChains.includes(networkArg)) {
+        console.log("Local network detected! Deploying mocks...");
+
+        await deploy("MockV3Aggregator", {
+            contract: "MockV3Aggregator",
+            from: deployer,
+            args: [DECIMALS, INITIAL_PRICE],
+            log: true,
+        });
+
+        console.log("Mocks Deployed!")
+        console.log("------------------------------------------------")
+    }
+    console.log("Mocks not Deployed!")
 }
+
+export const tags: string[] = ["all", "mocks"];
+export default func;
--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -5,7 +5,7 @@ type Config = {
     };
 }
 
-export const networkConfig: Config = {
+const networkConfig: Config = {
     43113: {
         name: "fuji",
         ethUsdPriceFeed: "0x86d67c3D38D2bCeE722E601025C25a575021c6EA"
@@ -14,4 +14,11 @@ export const networkConfig: Config = {
         name: "localhost",
         ethUsdPriceFeed: ""
     },
+}
+
+const developmentChains = ["hardhat", "localhost"];
+
+export {
+    networkConfig,
+    developmentChains
 }
